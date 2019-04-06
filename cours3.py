1be8c3c7fbbfca35f8ea4e5b6b88455a12791eab
KevinPautonnier <kevin.pautonnier@ynov.com>
2019-04-06 14:32:45 +0200

    update cours3.py

--- cours3.py
+++ cours3.py
@@ -7,25 +7,57 @@ digits = datasets.load_digits()
 data = digits['data']
 target  = digits['target']
 
-img0 = [data[idx] for idx,e in enumerate(target) if e == 0]
-img1 = [data[idx] for idx,e in enumerate(target) if e == 1]
+def OvO(x):
+    img0 = [data[idx] for idx,e in enumerate(target) if e == x]
+    for index in range(0, 10):
+        if(index != x):
+            img1 = [data[idx] for idx,e in enumerate(target) if e == index]
+                        
+            index_learn_0 = int(len(img0)*0.8)
+            index_test_0 = index_learn_0+1
 
-index_learn_0 = int(len(img0)*0.8)
-index_test_0 = index_learn_0+1
+            index_learn_1 = int(len(img1)*0.8)
+            index_test_1 = index_learn_1+1
 
-index_learn_1 = int(len(img1)*0.8)
-index_test_1 = index_learn_1+1
+            learn0, test0 = img0[index_learn_0:], img0[:index_test_0]
+            learn1, test1 = img1[index_learn_1:], img1[:index_test_1]
 
-learn0, test0 = img0[index_learn_0:], img0[:index_test_0]
-learn1, test1 = img1[index_learn_1:], img1[:index_test_1]
+            value0 = [0] * len(learn0)
+            value1 = [1] * len(learn1)
 
-value0 = [0] * len(learn0) 
-value1 = [1] * len(learn1)
+            learn01 = learn0 + learn1
+            value01 = value0 + value1
 
-learn01 = learn0 + learn1
-value01 = value0 + value1
+            reg = LogisticRegression(solver='lbfgs').fit(learn01, value01)
 
-reg_0_1 = LogisticRegression(solver='lbfgs').fit(learn01, value01)
+            #print(reg.score(learn01, value01))
+            print("Predicted :" , x if reg.predict([test1[0]]) else " other value", "Real value : ", x)
 
-print(reg_0_1.score(learn01, value01))
-print("Predicted :" , reg_0_1.predict([test0[0]]), "Real value : 0")
+
+def OvR(x):
+    img0 = [data[idx] for idx,e in enumerate(target) if e == x]
+    img1 = [data[idx] for idx,e in enumerate(target) if e != x]
+                
+    index_learn_0 = int(len(img0)*0.8)
+    index_test_0 = index_learn_0+1
+
+    index_learn_1 = int(len(img1)*0.8)
+    index_test_1 = index_learn_1+1
+
+    learn0, test0 = img0[index_learn_0:], img0[:index_test_0]
+    learn1, test1 = img1[index_learn_1:], img1[:index_test_1]
+
+    value0 = [0] * len(learn0)
+    value1 = [1] * len(learn1)
+
+    learn01 = learn0 + learn1
+    value01 = value0 + value1
+
+    reg = LogisticRegression(solver='lbfgs').fit(learn01, value01)
+
+    #print(reg.score(learn01, value01))
+    print("Predicted :" , x if reg.predict([test1[0]]) else " other value", "Real value : ", x)
+
+for i in range(0, 10):
+    OvR(i)
+    OvR(i)
