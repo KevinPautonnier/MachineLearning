ef9ea1622370f5d0465f310f283ba5eacf187e7f
KevinPautonnier <kevin.pautonnier@ynov.com>
2019-04-14 23:42:25 +0200

    complete OvO in cours3 and add OvR

--- cours3.py
+++ cours3.py
@@ -1,63 +1,66 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
+from itertools import combinations
+from sklearn.model_selection import train_test_split
+import operator
 
 digits = datasets.load_digits()
 
 data = digits['data']
 target  = digits['target']
 
-def OvO(x):
-    img0 = [data[idx] for idx,e in enumerate(target) if e == x]
-    for index in range(0, 10):
-        if(index != x):
-            img1 = [data[idx] for idx,e in enumerate(target) if e == index]
-                        
-            index_learn_0 = int(len(img0)*0.8)
-            index_test_0 = index_learn_0+1
+comb = list(combinations(set(target), 2))
 
-            index_learn_1 = int(len(img1)*0.8)
-            index_test_1 = index_learn_1+1
+def OvO(elem, dic, x_train, y_train):
+    img0 = [x_train[idx] for idx,e in enumerate(y_train) if e == elem[0]]
+    img1 = [x_train[idx] for idx,e in enumerate(y_train) if e == elem[1]]
+    
+    y = [1 for elem in img0]
+    y += [0 for elem in img1]
 
-            learn0, test0 = img0[index_learn_0:], img0[:index_test_0]
-            learn1, test1 = img1[index_learn_1:], img1[:index_test_1]
+    dic[elem] = LogisticRegression(solver='lbfgs').fit(img0+img1,y)
 
-            value0 = [0] * len(learn0)
-            value1 = [1] * len(learn1)
+def OvR(elem, dic, x_train, y_train):
+    img0 = [x_train[idx] for idx,e in enumerate(y_train) if e == elem]
+    img1 = [x_train[idx] for idx,e in enumerate(y_train) if e != elem]
+    
+    y = [1 for elem in img0]
+    y += [0 for elem in img1]
 
-            learn01 = learn0 + learn1
-            value01 = value0 + value1
-
-            reg = LogisticRegression(solver='lbfgs').fit(learn01, value01)
-
-            #print(reg.score(learn01, value01))
-            print("Predicted :" , x if reg.predict([test1[0]]) else " other value", "Real value : ", x)
+    dic[elem] = LogisticRegression(solver='lbfgs').fit(img0+img1,y)
 
 
-def OvR(x):
-    img0 = [data[idx] for idx,e in enumerate(target) if e == x]
-    img1 = [data[idx] for idx,e in enumerate(target) if e != x]
-                
-    index_learn_0 = int(len(img0)*0.8)
-    index_test_0 = index_learn_0+1
+dic = {}
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
+for elem in comb:
+    OvO(elem, dic, X_train, y_train)
 
-    index_learn_1 = int(len(img1)*0.8)
-    index_test_1 = index_learn_1+1
+correct_predict = 0
+for index,value in enumerate(X_test):
+    predict = {elem : 0 for elem in range(0,10)}
+    for key in dic:
+        predict[key[0] if dic[key].predict([value]) else key[1]] += 1
+    predicted_value = max(predict.items(), key=operator.itemgetter(1))[0]
+    # print("predict : ", predicted_value, "real value : ", y_test[index])
+    if(predicted_value == y_test[index]): correct_predict+=1  
+prct_predict = (correct_predict/len(X_test))*100
+print(f"Predict ratio : {prct_predict:.2f}")
 
-    learn0, test0 = img0[index_learn_0:], img0[:index_test_0]
-    learn1, test1 = img1[index_learn_1:], img1[:index_test_1]
+dic = {}
 
-    value0 = [0] * len(learn0)
-    value1 = [1] * len(learn1)
+for elem in set(target):
+    OvR(elem, dic, X_train, y_train)
 
-    learn01 = learn0 + learn1
-    value01 = value0 + value1
+correct_predict = 0
+for index,value in enumerate(X_test):
+    predict = {elem : 0 for elem in range(0,10)}
+    for key in dic:
+        predict[key] = dic[key].predict_proba([value])[0][1]
+            
+    predicted_value = max(predict.items(), key=operator.itemgetter(1))[0]
+    # print("predict : ", predicted_value, "real value : ", y_test[index])
+    if(predicted_value == y_test[index]): correct_predict+=1  
 
-    reg = LogisticRegression(solver='lbfgs').fit(learn01, value01)
-
-    #print(reg.score(learn01, value01))
-    print("Predicted :" , x if reg.predict([test1[0]]) else " other value", "Real value : ", x)
-
-for i in range(0, 10):
-    OvR(i)
-    OvR(i)
+prct_predict = (correct_predict/len(X_test))*100
+print(f"Predict ratio : {prct_predict:.2f}")
